21b2251dc10b179edc7d2273dd11b4be68dd0d52
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-03-18 13:22:52 -0300

    objetos criados

---
+++ Pokemon.Domain/Masters/Mestre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Domain.Masters
+{
+    public class Master
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
