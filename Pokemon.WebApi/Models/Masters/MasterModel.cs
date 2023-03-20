c59020cef49a083c2af6358b066d269e35f61905
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2023-03-20 09:44:23 -0300

    Models criado

---
+++ Pokemon.WebApi/Models/Masters/MasterModel.cs
@@ -0,0 +1,17 @@
+﻿using Pokemon.WebApi.Models.Pokemons;
+
+using System.Collections.Generic;
+
+namespace Pokemon.WebApi.Models.Masters
+{
+    public class MasterModel
+    {
+        public int Age { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+
+        public virtual List<PokemonModel> PokeList { get; set; }
+    }
+}
